247b66a37d392dad70b83e500acbeccfb9d6ec4a
Ramzi Dreessen <ramzidreessen@gmail.com>
2025-10-29 13:37:38 -0500

    Consolidate CSS

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -47,4 +47,142 @@ nav {
     }
   }
 }
+
+.logo,
+.logo .letter,
+.logo .letter>div {
+  -webkit-backface-visibility: hidden;
+  -webkit-transform-style: preserve-3d;
+  will-change: transform, opacity;
+}
+
+.logo,
+.logo .letter {
+  transition: transform 0.8s ease 0s, border-radius 3s ease 0s,
+    opacity 1s ease 0s;
+}
+
+.logo .r .top,
+.logo .r .bottom {
+  transition: transform 1s ease 0s, border-radius 1.3s ease 0.5s,
+    opacity 1s ease 0s;
+}
+
+.logo .k .top,
+.logo .k .bottom {
+  transition: transform 1s ease 0s, border-radius 1.6s ease 0.6s,
+    opacity 1s ease 0s;
+}
+
+.logo .d .top,
+.logo .d .bottom {
+  transition: transform 1s ease 0s, border-radius 1.8s ease 0.7s,
+    opacity 1s ease 0s;
+}
+
+.logo {
+  display: flex;
+  width: 150px;
+}
+
+.letter {
+  flex-grow: 1;
+}
+
+.letter:not(:first-of-type) {
+  margin-left: 2em;
+}
+
+.letter>div {
+  min-height: 1em;
+}
+
+.letter>div,
+.letter>div>div {
+  border: 0px solid black;
+  border-radius: 0px;
+  opacity: 0;
+}
+
+.logo {
+  transform: scaleX(0);
+}
+
+.loaded {
+
+  .letter,
+  .letter>div,
+  .letter>div>div {
+    opacity: 1 !important;
+  }
+
+  .logo {
+    transform: scaleX(1);
+
+    &.logoIsDimmed {
+      mix-blend-mode: lighten;
+    }
+  }
+
+  .r {
+    >div {
+      border-left: 10px solid black;
+      border-top-right-radius: 10em;
+    }
+
+    .top {
+      border-right: 10px solid black;
+      border-bottom-right-radius: 10em;
+    }
+
+    .bottom {
+      border-right: 10px solid black;
+    }
+  }
+
+  .k {
+    >div {
+      border-left: 10px solid black;
+      border-right: 10px solid black;
+    }
+
+    .top {
+      border-bottom-right-radius: 5em;
+    }
+
+    .bottom {
+      border-top-right-radius: 5em;
+    }
+  }
+
+  .d {
+    >div {
+      border-left: 10px solid black;
+      border-right: 10px solid black;
+    }
+
+    .top {
+      border-top-right-radius: 5em;
+    }
+
+    .bottom {
+      border-bottom-right-radius: 5em;
+    }
+  }
+}
+
+nav.loaded {
+  opacity: 1 !important;
+}
+
+nav {
+  transition: opacity 1s cubic-bezier(0.77, 0, 0.175, 1) 1.2s,
+    height 0.4s cubic-bezier(0.77, 0, 0.175, 1) 1.2s;
+}
+
+@media (prefers-reduced-motion: reduce) {
+  * {
+    transition: none !important;
+  }
+}
 </style>
