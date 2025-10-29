335bbad955071a80374fe3ab311f62a5d5be49a1
Ramzi Dreessen <ramzidreessen@gmail.com>
2025-10-29 18:51:07 -0500

    Get edge displaying on iphone

--- src/components/iPhone.vue
+++ src/components/iPhone.vue
@@ -5,14 +5,15 @@ export default {
 </script>
 
 <template>
-  <div class="iphone-video">
-    <video width="100%" autoplay loop muted playsinline src="@/assets/PD-mobile-walkthrough.mov"></video>
+  <div class="iphone-container">
+    <div class="iphone-video">
+      <video width="100%" autoplay loop muted playsinline src="@/assets/PD-mobile-walkthrough.mov"></video>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.iphone-video {
-  position: relative;
+.iphone-container {
   width: 250px;
   height: 540px;
   margin: 0 auto;
@@ -21,10 +22,20 @@ export default {
   -webkit-perspective-origin: center center;
   perspective-origin: center center;
 
-  &:hover>video {
+  &:hover .iphone-video {
     -webkit-transform: rotateY(-35deg);
     transform: rotateY(-35deg);
   }
+}
+
+.iphone-video {
+  position: relative;
+  width: 250px;
+  height: 522px;
+  -webkit-transform-style: preserve-3d;
+  transform-style: preserve-3d;
+  -webkit-transition: transform 0.6s ease-out;
+  transition: transform 0.6s ease-out;
 
   video {
     position: relative;
@@ -35,12 +46,6 @@ export default {
     border-radius: 45px;
     -webkit-filter: drop-shadow(5px 13px 9px rgba(0, 0, 0, .8));
     filter: drop-shadow(5px 13px 9px rgba(0, 0, 0, .8));
-    -webkit-transform-style: preserve-3d;
-    transform-style: preserve-3d;
-    -webkit-transition: transform 0.6s ease-out;
-    transition: transform 0.6s ease-out;
-    -webkit-backface-visibility: visible;
-    backface-visibility: visible;
     z-index: 2;
   }
 
@@ -49,41 +54,33 @@ export default {
     position: absolute;
     top: 0;
     left: 0;
-    width: 10px;
-    height: 540px;
+    width: 250px;
+    height: 522px;
     background-color: #a3a3a3;
-    background-image: -webkit-linear-gradient(left, hsl(0, 0%, 60%), hsl(0, 0%, 80%));
-    background-image: linear-gradient(to right, hsl(0, 0%, 60%), hsl(0, 0%, 80%));
-    -webkit-border-radius: 3px;
-    border-radius: 3px;
-    -webkit-transform: rotateY(90deg) translateZ(5px);
-    transform: rotateY(90deg) translateZ(5px);
-    -webkit-transform-origin: left center;
-    transform-origin: left center;
-    -webkit-transform-style: preserve-3d;
-    transform-style: preserve-3d;
+    background-image: -webkit-linear-gradient(top, hsl(0, 0%, 50%), hsl(0, 0%, 75%));
+    background-image: linear-gradient(to bottom, hsl(0, 0%, 50%), hsl(0, 0%, 75%));
+    -webkit-border-radius: 45px;
+    border-radius: 45px;
+    -webkit-transform: translateZ(-10px);
+    transform: translateZ(-10px);
     pointer-events: none;
-    z-index: 1;
+    z-index: 0;
   }
 
   &:after {
     content: '';
     position: absolute;
     top: 0;
-    right: 0;
+    left: 0;
     width: 10px;
-    height: 540px;
-    background-color: #b8b8b8;
-    background-image: -webkit-linear-gradient(right, hsl(0, 0%, 60%), hsl(0, 0%, 80%));
-    background-image: linear-gradient(to left, hsl(0, 0%, 60%), hsl(0, 0%, 80%));
-    -webkit-border-radius: 3px;
-    border-radius: 3px;
-    -webkit-transform: rotateY(-90deg) translateZ(5px);
-    transform: rotateY(-90deg) translateZ(5px);
+    height: 522px;
+    background-color: #1a1a1a;
+    background-image: -webkit-linear-gradient(left, hsl(0, 0%, 10%), hsl(0, 0%, 25%));
+    background-image: linear-gradient(to right, hsl(0, 0%, 10%), hsl(0, 0%, 25%));
+    -webkit-transform: translateX(-5px) rotateY(90deg);
+    transform: translateX(-5px) rotateY(90deg);
     -webkit-transform-origin: right center;
     transform-origin: right center;
-    -webkit-transform-style: preserve-3d;
-    transform-style: preserve-3d;
     pointer-events: none;
     z-index: 1;
   }
