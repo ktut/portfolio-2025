ab690f335b421b37dd66b908ab915086a3971401
Ramzi Dreessen <ramzidreessen@gmail.com>
2025-10-29 22:00:38 -0500

    Add content to site

--- src/views/Chase.vue
+++ src/views/Chase.vue
@@ -11,14 +11,26 @@ export default {
 
 <template>
   <div class="chase">
-    <div>
+    <div class="back-link">
       <RouterLink to="/">
-        back to home
+        &#x2190; Back to Home
       </RouterLink>
     </div>
-    <figure class="chase-image">
-      <img v-view-transition-name="'img'" alt="test image" src="../assets/Chase-portfolio-mock.jpg" style="width: 100%">
-    </figure>
+    <section class="chase-content">
+      <figure class="chase-image">
+        <img v-view-transition-name="'img'" alt="test image" src="../assets/Chase-portfolio-mock.jpg"
+          style="width: 100%">
+      </figure>
+      <h1>JPMorgan Chase Performance Dashboard</h1>
+      <h2>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua.
+      </h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua.
+      </p>
+    </section>
     <iPhone />
   </div>
 </template>
@@ -28,7 +40,17 @@ export default {
   padding: 20px;
 
   .chase-image {
-    max-width: 300px;
+    @media (min-width: 768px) {
+      width: 550px;
+      float: right;
+    }
+
+    max-width: 100%;
+
+    img {
+      width: 100%;
+      border-radius: 16px;
+    }
   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,7 +21,7 @@ export default {
       <figure>
         <img v-view-transition-name="'img'" src="../assets/Chase-portfolio-mock.jpg">
         <figcaption>
-          <span class="title">Chase Performance Dashboard</span>
+          <span class="title">JPMorgan Chase Performance Dashboard</span>
           <button class="cta">
             View Project <span class="unicode">&#x2197;</span></button>
         </figcaption>
