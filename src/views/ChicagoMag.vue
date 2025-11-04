a64c30a9449272fb3bbd01ed888b42d9724e04af
Ramzi Dreessen <ramzidreessen@gmail.com>
2025-11-04 09:58:20 -0600

    Update chimag design for larger screens

--- src/views/ChicagoMag.vue
+++ src/views/ChicagoMag.vue
@@ -71,51 +71,55 @@ export default {
         <p class="lead">Here's some of the work I did there:</p>
       </div>
 
-      <div class="video-bg">
-        <video src="../assets/vid-11.mov" autoplay loop muted playsinline />
+      <div class="video-container">
+        <div class="video-bg">
+          <video src="../assets/vid-11.mov" autoplay loop muted playsinline />
+        </div>
+        <div class="video-bg">
+          <video src="../assets/vid-7.mov" autoplay loop muted playsinline />
+        </div>
+        <div class="video-bg">
+          <video src="../assets/vid-8.mov" autoplay loop muted playsinline />
+        </div>
+        <div class="video-bg">
+          <video src="../assets/vid-3.mov" autoplay loop muted playsinline />
+        </div>
+        <div class="video-bg">
+          <video src="../assets/vid-5.mov" autoplay loop muted playsinline />
+        </div>
+        <div class="video-bg">
+          <video src="../assets/vid-1.mov" autoplay loop muted playsinline />
+        </div>
+        <div class="video-bg">
+          <video src="../assets/vid-6.mov" autoplay loop muted playsinline />
+        </div>
+        <div class="video-bg">
+          <video src="../assets/vid-9.mov" autoplay loop muted playsinline />
+        </div>
+        <div class="video-bg">
+          <video src="../assets/vid-4.mov" autoplay loop muted playsinline />
+        </div>
+        <div class="video-bg">
+          <video src="../assets/vid-10.mov" autoplay loop muted playsinline />
+        </div>
+        <div class="video-bg">
+          <video src="../assets/vid-12.mov" autoplay loop muted playsinline />
+        </div>
+        <div class="video-bg">
+          <video src="../assets/vid-13.mov" autoplay loop muted playsinline />
+        </div>
+        <div class="video-bg">
+          <video src="../assets/vid-14.mov" autoplay loop muted playsinline />
+        </div>
+        <div class="video-bg">
+          <video src="../assets/vid-2.mov" autoplay loop muted playsinline />
+        </div>
+        <div class="video-bg">
+          <video src="../assets/vid-15.mov" autoplay loop muted playsinline />
+        </div>
       </div>
-      <div class="video-bg">
-        <video src="../assets/vid-7.mov" autoplay loop muted playsinline />
-      </div>
-      <div class="video-bg">
-        <video src="../assets/vid-8.mov" autoplay loop muted playsinline />
-      </div>
-      <div class="video-bg">
-        <video src="../assets/vid-3.mov" autoplay loop muted playsinline />
-      </div>
-      <div class="video-bg">
-        <video src="../assets/vid-5.mov" autoplay loop muted playsinline />
-      </div>
-      <div class="video-bg">
-        <video src="../assets/vid-1.mov" autoplay loop muted playsinline />
-      </div>
-      <div class="video-bg">
-        <video src="../assets/vid-6.mov" autoplay loop muted playsinline />
-      </div>
-      <div class="video-bg">
-        <video src="../assets/vid-9.mov" autoplay loop muted playsinline />
-      </div>
-      <div class="video-bg">
-        <video src="../assets/vid-4.mov" autoplay loop muted playsinline />
-      </div>
-      <div class="video-bg">
-        <video src="../assets/vid-10.mov" autoplay loop muted playsinline />
-      </div>
-      <div class="video-bg">
-        <video src="../assets/vid-12.mov" autoplay loop muted playsinline />
-      </div>
-      <div class="video-bg">
-        <video src="../assets/vid-13.mov" autoplay loop muted playsinline />
-      </div>
-      <div class="video-bg">
-        <video src="../assets/vid-14.mov" autoplay loop muted playsinline />
-      </div>
-      <div class="video-bg">
-        <video src="../assets/vid-2.mov" autoplay loop muted playsinline />
-      </div>
-      <div class="video-bg">
-        <video src="../assets/vid-15.mov" autoplay loop muted playsinline />
-      </div>
+
+
     </section>
   </main>
 </template>
@@ -129,6 +133,19 @@ export default {
 
     figure {
       box-shadow: unset;
+    }
+  }
+
+  .content-wrapper {
+    @media (min-width: 1100px) {
+      max-width: 1600px;
+    }
+  }
+
+  .video-container {
+    @media (min-width: 1100px) {
+      column-count: 2;
+      column-gap: 50px;
     }
   }
 
